3629518683f52c0919582d8e27964b9ccd17b004
MASTERS <anusara.orm@gmail.com>
2017-11-14 15:37:50 +0700

    new test GG

---
+++ app/src/main/java/com/example/masters/fingerfutronic/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.masters.fingerfutronic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startfinger;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
