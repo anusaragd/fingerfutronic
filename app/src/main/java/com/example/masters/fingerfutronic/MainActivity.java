1ed47d527af54fb0dd8ba93b96f8817175ee5d1d
MASTERS <anusara.orm@gmail.com>
2017-11-14 15:48:15 +0700

    add interface

--- app/src/main/java/com/example/masters/fingerfutronic/MainActivity.java
+++ app/src/main/java/com/example/masters/fingerfutronic/MainActivity.java
@@ -3,15 +3,24 @@ package com.example.masters.fingerfutronic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button startfinger;
+    ImageView FingerImage;
+    TextView Message;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FingerImage = (ImageView) findViewById(R.id.imageView);
+        startfinger = (Button) findViewById(R.id.finger);
+        Message = (TextView) findViewById(R.id.status);
 
 
     }
